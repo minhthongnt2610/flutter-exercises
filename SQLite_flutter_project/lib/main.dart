afc385cf5d43364583dfc47b9cc2005660b68f84
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-08-15 22:01:13 +0700

    create new screen and set up environment

--- SQLite_flutter_project/lib/main.dart
+++ SQLite_flutter_project/lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqlite_flutter_project/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Text(
-        'qe',
-        style: TextStyle(
-          fontSize: 21,
-          fontWeight: FontWeight.bold,
-          color: Colors.pinkAccent,
-        ),
-      ),
-    );
-  }
 }
