4c9dca04fdc025b614a881a52981e05b212106f9
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-08-16 21:43:29 +0700

    create model and dbmodel

---
+++ SQLite_flutter_project/lib/models/friend_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqlite_flutter_project/models/db/db_friend_model.dart';
+
+class FriendModel {
+  const FriendModel({
+    @required this.id,
+    required this.name,
+    required this.phone,
+    required this.email,
+  });
+
+  final int? id;
+  final String name;
+  final String phone;
+  final String email;
+
+  FriendModel copyWith({int? id, String? name, String? phone, String? email}) {
+    return FriendModel(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      phone: phone ?? this.phone,
+      email: email ?? this.email,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'phone': phone, 'email': email};
+  }
+
+  @override
+  String toString() {
+    return 'FriendModel(id: $id, name: $name, phone: $phone, email: $email)';
+  }
+}
+
+extension FriendModelExtension on FriendModel {
+  DbFriendModel toDbFriendModel() {
+    return DbFriendModel(id: id, name: name, phone: phone, email: email);
+  }
+}
