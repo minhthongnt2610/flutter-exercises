1fa60d86adcbf237991442fd23f0429ca500d602
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-08-25 21:34:52 +0700

    update UI login_sign_up.dart

--- practice_firebase/lib/screens/login_sign_up_screen/widgets/filed_widget.dart
+++ practice_firebase/lib/screens/login_sign_up_screen/widgets/filed_widget.dart
@@ -1,34 +1,43 @@
 import 'package:flutter/material.dart';
-
-class FiledWidget extends StatelessWidget {
+class FiledWidget extends StatefulWidget {
   const FiledWidget({
     super.key,
     required this.labelText,
     required this.hintText,
     required this.isPassword,
+    required this.suffixIcon,
   });
   final String labelText;
   final String hintText;
   final bool isPassword;
+  final Widget suffixIcon;
   @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      cursorColor: Colors.white,
-      obscureText: isPassword,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: Colors.white.withOpacity(0.3),
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white, width: 2),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        labelText: labelText,
-        hintText: hintText,
-        labelStyle: TextStyle(color: Colors.white),
-        hintStyle: TextStyle(color: Colors.white),
-
-      ),
-      style: TextStyle(color: Colors.white),
-    );
-  }
+  State<FiledWidget> createState() => _FiledWidgetState();
 }
+
+class _FiledWidgetState extends State<FiledWidget> {
+
+    @override
+    Widget build(BuildContext context) {
+      return TextFormField(
+        cursorColor: Colors.white,
+        obscureText: widget.isPassword,
+        decoration: InputDecoration(
+          suffixIcon: widget.suffixIcon,
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.3),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 2),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          labelText: widget.labelText,
+          hintText: widget.hintText,
+          labelStyle: TextStyle(color: Colors.white),
+          hintStyle: TextStyle(color: Colors.white),
+
+        ),
+        style: TextStyle(color: Colors.white),
+      );
+    }
+  }
+
