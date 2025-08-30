870a4c1150588494df577c321efa06072d99c4a1
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-08-30 21:45:06 +0700

    create input_field.dart

---
+++ practice_firebase/lib/common_widgets/input_field.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+class InputField extends StatelessWidget
+{
+  const InputField({
+    super.key,
+    required this.hintText,
+    required this.onChanged,
+    required this.maxLines,
+    required this.initialValue,
+  });
+  final String hintText;
+  final ValueChanged<String> onChanged;
+  final int maxLines;
+  final String? initialValue;
+
+  @override
+  Widget build(BuildContext context)
+  {
+
+    // final controller = Provider.of<FriendController>(context, listen: true);
+    return TextFormField(
+      style:
+      const TextStyle(
+        color: Colors.white54,
+        fontSize: 16,
+      ),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.white30,
+          fontSize: 16,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        filled: true,
+        fillColor: Colors.white12,
+      ),
+
+      onChanged: onChanged,
+      maxLines: maxLines,
+      initialValue: initialValue,
+    );
+  }
+}
