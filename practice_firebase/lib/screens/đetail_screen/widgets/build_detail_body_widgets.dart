7924bf499371b7dad13be9fbc031000a3dc0930d
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-08-30 21:48:33 +0700

    update 30-8-2025

---
+++ practice_firebase/lib/screens/đetail_screen/widgets/build_detail_body_widgets.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class BuildDetailBodyWidgets extends StatefulWidget {
+  const BuildDetailBodyWidgets({super.key});
+
+  @override
+  State<BuildDetailBodyWidgets> createState() => _BuildDetailBodyWidgetsState();
+}
+
+class _BuildDetailBodyWidgetsState extends State<BuildDetailBodyWidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
