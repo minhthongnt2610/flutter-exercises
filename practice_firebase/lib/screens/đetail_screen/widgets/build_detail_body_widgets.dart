08bd5b34814a0b2468ded6c6df550465297f6996
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-01 21:00:36 +0700

    add delete_button.dart into detail_screen/widgets/build_detail_body_widget.dart

--- practice_firebase/lib/screens/đetail_screen/widgets/build_detail_body_widgets.dart
+++ practice_firebase/lib/screens/đetail_screen/widgets/build_detail_body_widgets.dart
@@ -5,23 +5,16 @@ import '../../../common_widgets/delete_button.dart';
 import '../../../common_widgets/input_field.dart';
 import '../../../common_widgets/primary_button.dart';
 
-
-
-class BuildDetailBodyWidget extends StatefulWidget
-{
-  const BuildDetailBodyWidget({super.key,});
+class BuildDetailBodyWidget extends StatefulWidget {
+  const BuildDetailBodyWidget({super.key});
 
   @override
   State<BuildDetailBodyWidget> createState() => _BuildDetailBodyWidgetState();
 }
 
-class _BuildDetailBodyWidgetState extends State<BuildDetailBodyWidget>
-{
-
-
+class _BuildDetailBodyWidgetState extends State<BuildDetailBodyWidget> {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     int width = MediaQuery.of(context).size.width.toInt();
     int height = MediaQuery.of(context).size.height.toInt();
     final isUpdate = true;
@@ -62,8 +55,7 @@ class _BuildDetailBodyWidgetState extends State<BuildDetailBodyWidget>
               onChanged: (value) {},
               maxLines: 1,
               initialValue: '',
-
-              ),
+            ),
             const SizedBox(height: 20),
             InputField(
               hintText: 'Address',
@@ -76,24 +68,22 @@ class _BuildDetailBodyWidgetState extends State<BuildDetailBodyWidget>
               title: isUpdate ? 'Update' : 'Add',
               isColor: true,
               onPressed: () {},
-
             ),
+            const SizedBox(height: 20),
+            DeleteButton(title: 'Delete', onTap: () {}),
           ],
         ),
       ),
     );
   }
 
-  Future<bool?> _showDeleteDialog(BuildContext context)
-  {
+  Future<bool?> _showDeleteDialog(BuildContext context) {
     return showDialog<bool>(
       context: context,
-      builder: (context)
-      {
+      builder: (context) {
         return AlertDialog(
           title: const Text('Delete Friend'),
-          content:
-          const Text('Are you sure you want to delete this friend?'),
+          content: const Text('Are you sure you want to delete this friend?'),
           actions: [
             TextButton(
               onPressed: () => Navigator.of(context).pop(false),
