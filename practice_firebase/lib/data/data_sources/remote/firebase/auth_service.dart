50089606123f8bf98e32c7d2143063eb35d864d9
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-08 22:03:26 +0700

    create isSignedIn

--- practice_firebase/lib/data/data_sources/remote/firebase/auth_service.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/auth_service.dart
@@ -2,4 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  bool isSignedIn(){
+    return _auth.currentUser != null ? true : false;
+
+  }
 }
