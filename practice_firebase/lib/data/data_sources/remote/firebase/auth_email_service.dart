9caaebe8cdb2e78dd67d7ae0bfd226fdd4ea1d92
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-12 22:38:25 +0700

    create auth_email_service.dart

--- practice_firebase/lib/data/data_sources/remote/firebase/auth_email_service.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/auth_email_service.dart
@@ -1,1 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthEmailService{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //đăng ký email và password
+  //đăng nhập bằng email và password
+  //đăng xuất
+  //xử lý lỗi firebase auth
+}
