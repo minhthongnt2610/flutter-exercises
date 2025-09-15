585c1d2246119dd105a5726022fbe19329c0de7f
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-15 22:30:33 +0700

    finish function clearUid

--- practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
@@ -12,7 +12,10 @@ class AuthSharedPrefs {
   // Ta lưu uid này vào SharedPreferences → để lần sau mở app, không cần đăng nhập lại (auto login).
 
   ///2. Xóa UID khi logout
-  static Future<void> clearUid() async {}
+  static Future<void> clearUid() async {
+      final prefs = await SharedPreferences.getInstance();
+        await prefs.remove(_uidKey);
+  }
   // Khi user bấm Đăng xuất, ta xóa UID khỏi SharedPreferences.
   // Điều này đảm bảo lần sau mở app, hệ thống biết user chưa đăng nhập.
 
