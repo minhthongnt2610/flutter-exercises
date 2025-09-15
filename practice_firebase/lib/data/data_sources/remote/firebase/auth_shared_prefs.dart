53a106b632a0544828fb00b307cf25fb99948273
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-15 22:28:37 +0700

    create function clearUid

--- practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
@@ -9,20 +9,24 @@ static Future<void> saveUid(String uid) async {
 
 // Khi user đăng nhập bằng Email / Google / Facebook, Firebase trả về một User (có uid).
 // Ta lưu uid này vào SharedPreferences → để lần sau mở app, không cần đăng nhập lại (auto login).
+
+
+///2. Xóa UID khi logout
+static Future<void> clearUid() async {}
+// Khi user bấm Đăng xuất, ta xóa UID khỏi SharedPreferences.
+// Điều này đảm bảo lần sau mở app, hệ thống biết user chưa đăng nhập.
+
+/// 3. Lấy UID đã lưu
+//
+// Khi app khởi động (ví dụ trong SplashScreen), ta kiểm tra xem SharedPreferences có uid không.
+// Nếu có → user đã đăng nhập trước đó → chuyển thẳng vào HomeScreen.
+// Nếu không → chuyển vào LoginScreen.
 }
 
 
 
 
 
-///2. Xóa UID khi logout
-//
-// Khi user bấm Đăng xuất, ta xóa UID khỏi SharedPreferences.
-// Điều này đảm bảo lần sau mở app, hệ thống biết user chưa đăng nhập.
-//
-/// 3. Lấy UID đã lưu
-//
-// Khi app khởi động (ví dụ trong SplashScreen), ta kiểm tra xem SharedPreferences có uid không.
-// Nếu có → user đã đăng nhập trước đó → chuyển thẳng vào HomeScreen.
-// Nếu không → chuyển vào LoginScreen.
+
+
 
