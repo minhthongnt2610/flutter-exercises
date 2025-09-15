0106ab986e4f9d5cae93ab2065f72f1818ffdf3f
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-15 22:30:11 +0700

    finish function saveUid

--- practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/auth_shared_prefs.dart
@@ -1,34 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class AuthSharedPrefs {
-    static const String _uidKey = 'uid';
+  static const String _uidKey = 'uid';
 
-/// 1. Lưu UID sau khi đăng nhập
+  /// 1. Lưu UID sau khi đăng nhập
+  static Future<void> saveUid(String uid) async {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString(_uidKey, uid);
+  }
+  // Khi user đăng nhập bằng Email / Google / Facebook, Firebase trả về một User (có uid).
+  // Ta lưu uid này vào SharedPreferences → để lần sau mở app, không cần đăng nhập lại (auto login).
 
-static Future<void> saveUid(String uid) async {
+  ///2. Xóa UID khi logout
+  static Future<void> clearUid() async {}
+  // Khi user bấm Đăng xuất, ta xóa UID khỏi SharedPreferences.
+  // Điều này đảm bảo lần sau mở app, hệ thống biết user chưa đăng nhập.
 
+  /// 3. Lấy UID đã lưu
+  static Future<String?> getSaveUid() async {}
+  // Khi app khởi động (ví dụ trong SplashScreen), ta kiểm tra xem SharedPreferences có uid không.
+  // Nếu có → user đã đăng nhập trước đó → chuyển thẳng vào HomeScreen.
+  // Nếu không → chuyển vào LoginScreen.
 }
-
-// Khi user đăng nhập bằng Email / Google / Facebook, Firebase trả về một User (có uid).
-// Ta lưu uid này vào SharedPreferences → để lần sau mở app, không cần đăng nhập lại (auto login).
-
-
-///2. Xóa UID khi logout
-static Future<void> clearUid() async {}
-// Khi user bấm Đăng xuất, ta xóa UID khỏi SharedPreferences.
-// Điều này đảm bảo lần sau mở app, hệ thống biết user chưa đăng nhập.
-
-/// 3. Lấy UID đã lưu
-static Future<String?> getSaveUid() async{
-
-}
-// Khi app khởi động (ví dụ trong SplashScreen), ta kiểm tra xem SharedPreferences có uid không.
-// Nếu có → user đã đăng nhập trước đó → chuyển thẳng vào HomeScreen.
-// Nếu không → chuyển vào LoginScreen.
-}
-
-
-
-
-
-
-
-
