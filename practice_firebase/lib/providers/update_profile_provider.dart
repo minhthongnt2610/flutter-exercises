046a640294f28ee812a212d1c3b6c6ea82f3fbe9
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-24 15:54:46 +0700

    create update_argument.dart

--- practice_firebase/lib/providers/update_profile_provider.dart
+++ practice_firebase/lib/providers/update_profile_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:practice_firebase/data/data_sources/remote/firebase/firestore_database/firestore_service.dart';
+import 'package:practice_firebase/models/friend_model.dart';
 
 class UpdateProfileProvider extends ChangeNotifier {
   final _firestoreService = FirestoreService();
@@ -11,4 +12,29 @@ class UpdateProfileProvider extends ChangeNotifier {
   DateTime? get birthday => _birthday;
   String? get email => _email;
 
+  void setName(String value) {
+    _name = value;
+    notifyListeners();
+  }
+
+  void setBirthday(DateTime value) {
+    _birthday = value;
+    notifyListeners();
+  }
+
+  void setEmail(String value) {
+    _email = value;
+    notifyListeners();
+  }
+
+  Future<String?> updateProfile() async {
+    try {
+      _firestoreService.updateFriend(
+        FriendModel(id: 1, name: _name, birthdate: _birthdate, email: _email),
+      );
+      return null;
+    } catch (e) {
+      return e.toString();
+    }
+  }
 }
