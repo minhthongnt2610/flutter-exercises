1375871d211adeb0528ebe541b84266c8de41301
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-24 14:36:58 +0700

    String get uid => _auth.currentUser!.uid;

--- practice_firebase/lib/data/data_sources/remote/firebase/firestore_database/firestore_email_service.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/firestore_database/firestore_email_service.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirestoreEmailService{
+class FirestoreEmailService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  String get uid => _auth.currentUser!.uid;
 }
