3e88a13860f7d7c02e45fe5506dbb443713501a4
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-24 13:25:33 +0700

    finish input_date_time_field.dart

---
+++ practice_firebase/lib/screens/update_profile/widgets/input_date_time_field.dart
@@ -0,0 +1,36 @@
+import 'package:date_field/date_field.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class InputDateTimeField extends StatelessWidget
+{
+  const InputDateTimeField({
+    super.key,
+    required this.selectedDate,
+    required this.onChanged
+  });
+  final DateTime? selectedDate;
+  final ValueChanged<DateTime?> onChanged;
+  @override
+  Widget build(BuildContext context)
+  {
+    return DateTimeField(
+      decoration: const InputDecoration(
+        labelText: 'Enter Date',
+        labelStyle: TextStyle(
+          color: Colors.white30,
+          fontSize: 16,
+        ),
+        helperText: 'YYYY/MM/DD',
+        border: OutlineInputBorder(),
+        filled: true,
+        fillColor: Colors.white12,
+      ),
+      value: selectedDate,
+      dateFormat: DateFormat.yMd(),
+      mode: DateTimeFieldPickerMode.date,
+      initialPickerDateTime: DateTime(2001, 11, 20),
+      onChanged: onChanged,
+    );
+  }
+}
