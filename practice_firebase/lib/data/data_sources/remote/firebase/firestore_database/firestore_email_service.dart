ed4d8f30a47b9d0757bbe41c0e135ad918498c8d
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-24 14:08:11 +0700

    create firestore_email_service.dart

---
+++ practice_firebase/lib/data/data_sources/remote/firebase/firestore_database/firestore_email_service.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirestoreEmailService{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+}
