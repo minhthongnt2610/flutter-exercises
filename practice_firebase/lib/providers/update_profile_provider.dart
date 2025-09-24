93feba6ba6b6f7b1334eaf99f676456df93bc38b
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-24 15:29:08 +0700

    create property of update_profile_provider.dart

--- practice_firebase/lib/providers/update_profile_provider.dart
+++ practice_firebase/lib/providers/update_profile_provider.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/widgets.dart';
+import 'package:practice_firebase/data/data_sources/remote/firebase/firestore_database/firestore_service.dart';
 
-class UpdateProfileProvider extends ChangeNotifier{
+class UpdateProfileProvider extends ChangeNotifier {
+  final _firestoreService = FirestoreService();
+  String? _name;
+  DateTime? _birthday;
+  String? _email;
+
+  String? get name => _name;
+  DateTime? get birthday => _birthday;
+  String? get email => _email;
 
 }
