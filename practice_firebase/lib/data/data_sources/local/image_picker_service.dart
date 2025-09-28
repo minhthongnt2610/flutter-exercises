0016980f4be9c3457f31ead3f1edca62c5fef75c
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-09-28 21:49:37 +0700

    create _pickFromGallery in image_picker_service.dart

--- practice_firebase/lib/data/data_sources/local/image_picker_service.dart
+++ practice_firebase/lib/data/data_sources/local/image_picker_service.dart
@@ -1,1 +1,21 @@
-c
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+class ImagePickerService{
+  final ImagePicker _picker = ImagePicker();
+  Future<void> _pickFromGallery(BuildContext context) async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+      if (!context.mounted) return;
+      Navigator.pop(context, File(image.path));
+    } catch (e) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Lỗi khi chọn ảnh: $e')));
+      }
+    }
+  }
+}
