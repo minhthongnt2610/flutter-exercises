3bda4ff0e7fded68d1cac824543ebb869aa9421c
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-04 17:51:38 +0700

    finish new_friend_screen_argument.dart

---
+++ practice_firebase/lib/screens/detail_screen/models/new_friend_screen_argument.dart
@@ -0,0 +1,6 @@
+import 'package:practice_firebase/models/friend_model.dart';
+
+class NewFriendScreenArgument {
+  final FriendModel? friendModel;
+  const NewFriendScreenArgument({this.friendModel});
+}
