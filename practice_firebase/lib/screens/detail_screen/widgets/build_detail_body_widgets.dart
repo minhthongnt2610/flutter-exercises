cca84c01c4017b934bd89cb1e6e36834dd585dc6
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-06 23:16:39 +0700

    update build_detail_body_widgets.dart

--- practice_firebase/lib/screens/detail_screen/widgets/build_detail_body_widgets.dart
+++ practice_firebase/lib/screens/detail_screen/widgets/build_detail_body_widgets.dart
@@ -140,7 +140,6 @@ class _BuildDetailBodyWidgetState extends State<BuildDetailBodyWidget> {
                       _authEmailService.currentUser!.uid,
                   );
 
-
                   }
 
                   if (context.mounted) {
