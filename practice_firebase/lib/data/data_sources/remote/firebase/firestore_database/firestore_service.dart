9346b5781c0f33a0316c21cea676471b5af5a52a
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-07 14:38:00 +0700

    delete import is not important

--- practice_firebase/lib/data/data_sources/remote/firebase/firestore_database/firestore_service.dart
+++ practice_firebase/lib/data/data_sources/remote/firebase/firestore_database/firestore_service.dart
@@ -1,5 +1,4 @@
 import "package:cloud_firestore/cloud_firestore.dart";
-import "package:firebase_auth/firebase_auth.dart";
 
 import "../../../../../models/firebase/fb_friend_model.dart";
 import "../../../../../models/firebase/fb_user_model.dart";
@@ -79,6 +78,5 @@ class FirestoreService {
         .doc(userid)
         .snapshots()
         .map((doc) => FbUserModel.fromJson(doc.data()!, doc.id));
-
   }
 }
