6fcdba39c3c7664a7683782ae8598706912ff998
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-07 14:18:42 +0700

    add fromJson and toJson

--- practice_firebase/lib/models/user_model.dart
+++ practice_firebase/lib/models/user_model.dart
@@ -5,5 +5,18 @@ class UserModel {
 
   UserModel({required this.id, required this.nameUser, required this.photoUrl});
 
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    id: json["id"],
+    nameUser: json["nameUser"],
+    photoUrl: json["photoUrl"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "nameUser": nameUser,
+    "photoUrl": photoUrl,
+  };
+
 
 }
+
