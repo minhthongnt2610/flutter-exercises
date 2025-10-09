2281f2b78a31848aacf3ddd45addf929f4e5dca1
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-09 23:36:45 +0700

    update

--- practice_firebase/lib/screens/avatar_selection_screen/avatar_selection_screen.dart
+++ practice_firebase/lib/screens/avatar_selection_screen/avatar_selection_screen.dart
@@ -20,7 +20,6 @@ class AvatarSelectionScreen extends StatelessWidget {
     "https://i.pravatar.cc/150?img=10",
     "https://i.pravatar.cc/150?img=11",
     "https://i.pravatar.cc/150?img=12",
-    "https://i.pravatar.cc/150?img=13",
 
   ];
 
