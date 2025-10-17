34a666e9c9f167cca52fddfc8455f0ac07047c1e
Nguyễn Minh Thông <128831784+minhthongnt2610@users.noreply.github.com>
2025-10-17 20:56:34 +0700

    update button login of login_screen.dart

--- practice_firebase/lib/screens/login_screen/login_screen.dart
+++ practice_firebase/lib/screens/login_screen/login_screen.dart
@@ -145,8 +145,9 @@ class LoginBody extends StatelessWidget {
                       onPressed: () async {
                         if (_formKey.currentState!.validate()) {
                           await loginProvider.login();
-
-                          Navigator.pushNamed(context, HomeScreen.routeName);
+                          Navigator.of(
+                            context,
+                          ).pushNamedAndRemoveUntil(LoginScreen.routeName, (route) => false);
                         }
                       },
                     ),
